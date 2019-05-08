691cf1dffe5c68f2a893df37ea23716c6b8deb5c
Joshua Martinez <joshkmartinez@gmail.com>
2019-05-07 21:49:57 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {}
+  render() {
+    return (
+      <React.Fragment>
+        <div>RPS</div>
+      </React.Fragment>
+    )
+  }
+}
+export default App
